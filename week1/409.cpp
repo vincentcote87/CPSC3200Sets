9850a15f4038363bec7c259e6a56e86068111c2c
vincentcote87 <vincentcote87@gmail.com>
2020-01-13 16:02:14 -0700

    409 Accepted

--- week1/409.cpp
+++ week1/409.cpp
@@ -3,39 +3,54 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 int main(void) {
     int numOfWords;
     int numOfExcuses;
     int count = 0;
+    int worst = 0;
+    int index = 0;
     std::string temp;
     std::string worstExcuse;
+    std::string currentExcuse;
     std::vector<std::string> words;
-    std::vector<std::string> excuses;
 
-    while (!std::cin.eof()) {
+    while (true) {
+        index++;
+        count = -1;
+        worst = -1;
+        worstExcuse = "";
+        words.clear();
         std::cin>>numOfWords>>numOfExcuses;
+        if (std::cin.eof())
+          break;
         for (int i = 0; i < numOfWords; i++) {
             std::cin>>temp;
             words.push_back(temp);
         }
+        std::cin.ignore();
         for (int i = 0; i < numOfExcuses; i++) {
-            std::getline(std::cin, temp);
-
-            std::istringstream iss(temp);
-            while (iss >> temp) {
-                for (int i = 0; i < temp.size(); i++) {
-                    isalpha(temp[i]) ? temp[i] = tolower(temp[i]) : temp[i] = ' ';
-                    // std::cout<<temp[i]<<std::endl;
-                }
-                std::cout<<temp<<std::endl;
+          count = 0;
+          std::getline(std::cin, temp);
+          currentExcuse = temp;
+          for (int j = 0; j < temp.length(); j++) {
+            isalpha(temp[j]) ? temp[j] = tolower(temp[j]) : temp[j] = ' ';
+          }
+          std::istringstream iss(temp);
+          while (iss >> temp) {
+            if (std::find(words.begin(), words.end(), temp) != words.end()) {
+              count++;
             }
-            // excuses.push_back(temp);
-        }
-
-        // std::cout<<numOfWords<<" "<<numOfExcuses<<std::endl;
+          }
+          if (count > worst) {
+            worst = count;
+            worstExcuse = "\n" + currentExcuse;
+          } else if (count == worst)
+            worstExcuse += "\n" + currentExcuse;
+      }
+      std::cout<<"Excuse Set #" + std::to_string(index);
+      std::cout<<worstExcuse<<std::endl<<std::endl;
     }
-
-
     return 0;
 }
