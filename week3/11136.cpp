64f3ea09f770af72d1b31c3a8b2f80a5190a290e
Vincent <vincent.cote@uleth.ca>
2020-01-26 16:39:10 -0700

    11136 accepted

--- week3/11136.cpp
+++ week3/11136.cpp
@@ -1,1 +1,32 @@
-// multiset
+// Problem 11136 Hoax or What
+// Vincent Cote - CPSC 3200
+// Approach - Using a multiset as the data structure, while iterating through the bills take the front and back of the multiset
+// to get the difference which is added to a running total then remove them from the set.
+#include <iostream>
+#include <set>
+
+int main(void) {
+	int days;
+	while (true) {
+		std::cin>>days;
+		if (days == 0)
+			return 0;
+		std::multiset<int> mSet;
+		long long totalPaid = 0;
+		while (days--) {
+			int k;
+			std::cin>>k;
+			while(k--) {
+				int temp;
+				std::cin>>temp;
+				mSet.insert(temp);
+			}
+			int lowest = *mSet.begin();
+			int highest = *(--mSet.end());
+			totalPaid += highest - lowest;
+			mSet.erase(mSet.begin());
+			mSet.erase(--mSet.end());
+		}
+		std::cout<<totalPaid<<std::endl;
+	}
+}
